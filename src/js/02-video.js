97ee951fa6678d358c414547954ff8104eff45d1
Ivan Shvayka <shvaykaofficial@gmail.com>
2022-07-21 18:39:32 +0300

    add tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+let LOCALSTORAGE_KEY = 'videoplayer-current-time'
+
+player.on('timeupdate', throttle(setSeconds, 1000));
+
+function setSeconds({ seconds }) {
+    localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+}
+
+const savedTime = localStorage.getItem(LOCALSTORAGE_KEY)
+if (savedTime) {player.setCurrentTime(savedTime)}
